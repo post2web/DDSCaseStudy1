26d203aeafea0dbe498c33fb02e36a0d8b5da39e
Ivelin Angelov <post2web@gmail.com>
2017-03-09 08:33:30 -0500

    changes are made in order to download form original the soruce and place files in data folder

--- source/download.R
+++ source/download.R
@@ -1,32 +1,14 @@
-#For reproducibility using Rstudio Version 1.0.136
-
-#R code to clean the data that is called from CleanCaseStudyMakefile
-#Clean EDS and GDP data for Case Study 1 in class Doing Data Science 6306-404
-#Goal is to use Country Short Name in column 'AE' in the EDS STATS Country file as the main sort key to answer questions about income.
-
-#This needs to be unique for each person who downloads and runs the data. 
-#setwd("/Users/laurabishop/Documents/Git Repositories/DDSCaseStudy1/data")
-
-#Download ESD data
+# R code to download csv files for the project
 
 install.packages("downloader") 
-library (downloader)
-install.packages ("dplyr")
-library(dplyr)
-install.packages("RCurl")
-library(RCurl)
+require(downloader)
 
-download.file ("https://raw.githubusercontent.com/post2web/DDSCaseStudy1/master/data/EDSTATS_Country.csv", 
-							 destfile ="EDSTATS_CountryDown.csv")
-cat ("EDS Country Data downloaded\n")
-
-#Read EDS Country file
-EDSraw <- read.csv ("EDSTATS_CountryDown.csv", header = TRUE, sep = ',')
-cat ("EDS read into raw data file - EDSraw\n")
+#Download EDS Country data
+download.file(url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv',
+              destfile = "../data/EDSTATS_Country.csv")
+cat("EDS Country Data downloaded")
 
 #Download GDP data
-download.file ("https://raw.githubusercontent.com/post2web/DDSCaseStudy1/master/data/GDP.csv", 
-							 destfile ="GDPdown.csv")
-#Read GDP Country file
-GDPraw <- read.csv ("GDPdown.csv", header = TRUE, sep = ',')
-cat ("GDP read into raw data file - GDPraw\n")
+download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", 
+							destfile ="../data/GDP.csv")
+cat("GDP Data downloaded")
