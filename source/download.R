1f9705ad186e994d2cd69bdd2bb20eb08b518503
Ivelin Angelov <post2web@gmail.com>
2017-03-09 08:58:09 -0500

    fix to work on windows

--- source/download.R
+++ source/download.R
@@ -3,12 +3,18 @@
 install.packages("downloader") 
 require(downloader)
 
+if (.Platform$OS.type == 'unix') {
+  data_path = '../data/'
+} else {
+  data_path = '..\\data\\'
+}
+
 #Download EDS Country data
 download.file(url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv',
-              destfile = "../data/EDSTATS_Country.csv")
+              destfile = paste(data_path, "EDSTATS_Country.csv"))
 cat("EDS Country Data downloaded")
 
 #Download GDP data
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", 
-							destfile ="../data/GDP.csv")
+							destfile = paste(data_path, "GDP.csv"))
 cat("GDP Data downloaded")
