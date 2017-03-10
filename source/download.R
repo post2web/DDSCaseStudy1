608013920ed88107c8efde21a74d29084c17d07f
Ethan Graham <gramcraka@gmail.com>
2017-03-09 18:22:15 -0800

    Optimization for download.R

--- source/download.R
+++ source/download.R
@@ -3,19 +3,12 @@
 install.packages("downloader", repos = structure(c(CRAN="http://cran.r-project.org"))) 
 require(downloader)
 
-if (.Platform$OS.type == 'unix') {
-  data_path = '../data/'
-} else {
-  data_path = '..\\data\\'
-}
-
 #Download EDS Country data
 download.file(url = 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv',
-              destfile = paste(data_path, "EDSTATS_Country.csv", sep = ''))
+              destfile = file.path("..", "data", "EDSTATS_Country.csv"))
 cat("EDS Country Data downloaded")
 
 #Download GDP data
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", 
-							destfile = paste(data_path, "GDP.csv", sep = ''))
-
+							destfile = file.path("..", "data", "GDP.csv"))
 cat("GDP Data downloaded\n")
